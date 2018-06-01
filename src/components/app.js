d2ccf0fca3e373b8253332bf810795594da1c53d
Alejandro <alejovp@gmail.com>
2018-06-01 22:55:25 +0200

    Setting SearchBar as controlled component and binding context

--- src/components/app.js
+++ src/components/app.js
@@ -5,7 +5,7 @@ import SearchBar from '../containers/SearchBar';
 export default class App extends Component {
   render() {
     return (
-      <div>
+      <div className="container">
         <div>React simple starter</div>
         <SearchBar />
       </div>
--- src/containers/SearchBar.js
+++ src/containers/SearchBar.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react';
 
-class SearchBar extends Component {
+export default class SearchBar extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = { term: '' };
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+
+  onInputChange(event) {
+    console.log(event.target.value);
+    this.setState({ term: event.target.value })
+  }
+  
   render() {
     return (
       <form className="input-group">
-        <input className="form-control"/>
+        <input 
+          type="text" 
+          className="form-control"
+          placeholder="Write a City and get a 5 day forecast"
+          value={this.state.term}
+          onChange={this.onInputChange}/>
+        <div className="input-group-append">
+          <button className="btn btn-outline-secondary" type="submit">Search</button>
+        </div>
       </form>
     );
   }
 }
-
-export default SearchBar;
