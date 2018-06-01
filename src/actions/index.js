0619bc05b449b50042dde800dfc1df5a4b909fad
Alejandro <alejovp@gmail.com>
2018-06-02 00:26:21 +0200

    Preparing the action creator with axios

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+import Constants from '../constants';
+
+const API_BASE_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${Constants.API_WEATHER_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${API_BASE_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  }
+}
